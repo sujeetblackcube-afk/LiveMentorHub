beea1eba311af1e95088154c86fde341e17595dc
Praveen Reddy <praveenreddyg5612@gmail.com>
2026-07-17 13:52:19 +0530

    dashbord route issue

--- backend/routes/dashboardRoute.js
+++ backend/routes/dashboardRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getDashboardStats } from "../controllers/dashboardController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
