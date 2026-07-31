04376b312d76b0b751a2c8c587929e912023134a
Praveen Reddy <praveenreddyg5612@gmail.com>
2026-07-31 10:43:31 +0530

    build

--- student/src/app/courses/[courseId]/page.tsx
+++ student/src/app/courses/[courseId]/page.tsx
@@ -480,7 +480,7 @@ export default function CourseDetailsPage() {
                                                             >
                                                                 <Download className="h-4 w-4 mr-1" />
                                                                 Download
-                                                            </a>
+                                                            </button>
                                                         </div>
                                                     </div>
                                                 ))}
